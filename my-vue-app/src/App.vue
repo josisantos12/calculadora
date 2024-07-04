2d88fd2f044191feceff13208a4632849eca20be
josisantos12 <josiane15santos24@gmail.com>
2024-07-04 16:06:44 -0300

    calculadora

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <MinhaCalculadora />
+  </div>
+</template>
+
+<script>
+import MinhaCalculadora from './components/minhaCalculadora.vue';
+
+export default {
+  components: { MinhaCalculadora }
+};
+</script>
---
+++ my-vue-app/src/components/minhaCalculadora.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+      <input type="number" v-model="num1" />
+      <select v-model="operation">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input type="number" v-model="num2" />
+      <p>Resultado: {{ result }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        num1: 0,
+        num2: 0,
+        operation: "+",
+        result: 0
+      };
+    },
+    watch: {
+      num1() {
+        this.calculate();
+      },
+      num2() {
+        this.calculate();
+      },
+      operation() {
+        this.calculate();
+      }
+    },
+    methods: {
+      calculate() {
+        switch (this.operation) {
+          case "+":
+            this.result = this.num1 + this.num2;
+            break;
+          case "-":
+            this.result = this.num1 - this.num2;
+            break;
+          case "*":
+            this.result = this.num1 * this.num2;
+            break;
+          case "/":
+            if (this.num2 !== 0) {
+              this.result = this.num1 / this.num2;
+            } else {
+              this.result = "Erro: divisão por zero";
+            }
+            break;
+        }
+      }
+    }
+  };
+  </script>
